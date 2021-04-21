121573066c1a68dc1bc78f214feb0f830063a2ad
darya <darya.suhodeeva@yandex.ru>
2021-04-21 16:23:02 +0700

    z

--- HW_6/Program.cs
+++ HW_6/Program.cs
@@ -6,27 +6,22 @@ namespace HW_6
     {
         static void Main(string[] args)
         {
-            Test();
-        }
-
-        public static void Test()
-        {
             Counter account = new Counter();
 
             account.Notify += CounterDelegate;
             account.Notify2 += CounterDelegate2;
-
-            static void CounterDelegate(object sender, Handler1 e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
-            static void CounterDelegate2(object sender, Handler2 e)
-            {
-                Console.WriteLine(e.Message);
-            }
-
             account.Count();
         }
+
+        static void CounterDelegate(object sender, Handler1 e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        static void CounterDelegate2(object sender, Handler2 e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
     }
 }
